b684f1082ef1da7fe4e1a677108b8c9257106cfd
RIYA <riya@SREEKESH>
2013-07-23 13:57:20 +0530

    First commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php include ("include/header.php"); ?>
+
+<!--<div id="navigation"><div id='cssmenu'></div></div>-->
+
+<div id="wrapper_top"></div>
+<div id="wrapper">
+
+<div class="col_lft">
+<span style="padding: 10px 0 40px 0;">
+<img src="images/map.png" alt="map" width="355" height="189" class="border0"/></span>
+<span style="padding: 10px 0;">
+<a href="admin/"><img src="images/login_admin.jpg" alt="admin" width="355" height="60" class="border0"/></a></span>
+<span style="padding: 10px 0;">
+<a href="employee/login.php"><img src="images/login_emp.jpg" alt="emp" width="355" height="60" class="border0"/></a></span></div>	
+
+<div class="col_right">
+<h1>Overseas Education</h1>
+<p>
+International education can mean many different things and its definition is debated. Some have defined two general meanings according to its involvement of students. The first refers to education that transcends national borders by the exchange of people, for example, by students travelling to study at an international branch campus, as part of a study abroad program or as part of a student exchange program. The second is a comprehensive approach to education that intentionally prepares students to be active and engaged participants in an interconnected world.
+</p>
+<h1>About Riya Education</h1>
+<p>
+Riya Education provides tremendous opportunity for students to experience a global exposure. We extend guidance & assistance to students for overseas education. Our core activity lies in assisting students to make the right choice with regard to pursuing education abroad.
+</p>
+<h4>Accreditation</h4>
+<p>
+Riya Travel & Tours and Riya Holidays are accredited by IATA (International Air Transport Association) and is a member of TAAI (Travel Agents Association of India), TAFI (Travel Agents Federation of India) and IATO (Indian Association of Tour Operators).
+</p>
+
+</div>	
+
+</div>
+
+<?php include("include/footer.php"); ?>
+
+
+</div> <!--end of container-->
+</body>
+</html>
